ba9d52523e356c1cea19f0cd7d00a7cf4fea13dd
Khoa Le <khoale177@gmail.com>
2021-10-13 22:53:02 -0700

    Authentication Functions

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -57,6 +57,11 @@ namespace BugTracker.Areas.Identity.Pages.Account
 
         public async Task OnGetAsync(string returnUrl = null)
         {
+            if (User.Identity.IsAuthenticated)
+            {
+                Response.Redirect("/Home");
+            }
+
             if (!string.IsNullOrEmpty(ErrorMessage))
             {
                 ModelState.AddModelError(string.Empty, ErrorMessage);
